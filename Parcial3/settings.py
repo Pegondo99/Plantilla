d16b2355d3dd6492e9e9930d2eec9931b0dd01ba
Pegondo99 <pegondo99@uma.es>
2021-01-14 21:44:28 +0000

    Cambiado sesion_engine

--- Parcial3/settings.py
+++ Parcial3/settings.py
@@ -107,7 +107,7 @@ DATABASES = {
 
 mongoengine.connect(host=NOMBRE_DB)
 
-SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
