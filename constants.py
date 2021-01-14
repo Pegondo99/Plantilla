3c74805951d0acf75001a7cb150a4d1c6551a64f
Pegondo99 <pegondo99@uma.es>
2021-01-14 17:18:31 +0000

    Sin barra

--- constants.py
+++ constants.py
@@ -1,3 +1,2 @@
 #APP_NAME = "http://localhost:8000"
-APP_NAME = "https://plantillaexameningweb.herokuapp.com/"
-#APP_NAME = "https://parcial3-stela.herokuapp.com"
+APP_NAME = "https://plantillaexameningweb.herokuapp.com"
