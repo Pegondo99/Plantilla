b2107001071686c23123f8ba058d1a86d411d547
Pegondo99 <pegondo99@uma.es>
2021-01-14 17:05:28 +0000

    cambios

--- constants.py
+++ constants.py
@@ -1,2 +1,3 @@
-APP_NAME = "http://localhost:8000"
+#APP_NAME = "http://localhost:8000"
+APP_NAME = "https://plantillaexameningweb.herokuapp.com/"
 #APP_NAME = "https://parcial3-stela.herokuapp.com"
