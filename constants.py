7b6d99e819e40fe800aa40c3e6536de169b067aa
Pegondo99 <pegondo99@uma.es>
2021-01-15 18:24:53 +0000

    cambiadas urls

--- constants.py
+++ constants.py
@@ -1,2 +1,2 @@
-APP_NAME = "http://localhost:8000"
-#APP_NAME = "https://plantillaexameningweb.herokuapp.com"
+#APP_NAME = "http://localhost:8000"
+APP_NAME = "https://plantillaexameningweb.herokuapp.com"
